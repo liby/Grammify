b9938846a2062802b008b4cf1eb7c8179606315f
Bryan Lee <38807139+liby@users.noreply.github.com>
2023-03-07 16:20:58 +0800

    👷 Fix ci and disable code minimize

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  },
   images: {
     unoptimized: true,
   },
